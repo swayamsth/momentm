beaae309f6eac0cb482959bd8af7725e9afe8cf8
Sam. <swayams24@gmail.com>
2025-12-13 20:30:42 +1100

    Add Start Button to the homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,8 @@
+import StartTransformationBtn from "@/components/StartTransformationBtn";
+
 const Home = () => {
   return (
-    <section className="bg-[url('/hero-background.webp')] min-h-screen w-full bg-cover bg-center relative">
+    <section className="bg-[url('/hero-background.webp')] min-h-screen w-full bg-cover bg-center relative text-white">
       <div className="grid place-content-center gap-y-3 p-8 w-full h-full top-0 absolute text-center">
         <h1 className="font-satoshi text-2xl font-normal leading-tight md:text-4xl xl:text-5xl 2xl:text-6xl mx-auto">
           The <span className="font-medium">Fitness App</span> That Feels Like
@@ -10,6 +12,7 @@ const Home = () => {
           Smarter habit-building. Adaptive training. Personalized nutrition. All
           powered by an AI coach that never stops leveling up.
         </p>
+        <StartTransformationBtn />
       </div>
     </section>
   );
